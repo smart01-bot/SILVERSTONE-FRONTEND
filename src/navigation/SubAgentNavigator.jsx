71b7601f22be117d4c073bbf928656b8ce921af4
smartventurecapitalinc <smartventurecapitalinc@gmail.com>
2026-05-13 15:40:55 +0000

    fix: duplicate RequestSuccessScreen import

--- src/navigation/SubAgentNavigator.jsx
+++ src/navigation/SubAgentNavigator.jsx
@@ -1,5 +1,4 @@
 // src/navigation/SubAgentNavigator.jsx
-import RequestSuccessScreen from '../screens/sub-agent/RequestSuccessScreen';
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
